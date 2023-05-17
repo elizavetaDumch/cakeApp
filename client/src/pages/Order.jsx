8ca8cd8c52fdd0a68499bc219f7a6b83896ba912
elizavetaDumch <dumcheli@mail.ru>
2023-05-17 18:25:51 +0300

    Update Order.jsx

--- client/src/pages/Order.jsx
+++ client/src/pages/Order.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import {Container, Row} from "react-bootstrap";
+import { Container, Row } from "react-bootstrap";
 import { useParams } from "react-router-dom";
 import { fetchOrder } from "../http/orderAPI";
 import Spinner from "../components/Spinner";
@@ -19,18 +19,27 @@ const Order = () => {
         });
     }, []);
 
+    var quantity
+    if (order.order_products) {
+        quantity = order.order_products
+            .map((product) => product.quantity)
+            .reduce((previous, current) => previous + current, 0)
+    }
+
     const orderDetails = (
         <> <br></br>
-        <Container className="d-flex flex-column rounded border bg-white"  style={{width:"400px", marginTop: "100px"}}>
-            <h4 className="text-center mt-3">Ваш заказ успешно сформирован</h4>
-            <div className="my-4">
-                <div>Номер заказа: <b>№{order.id}</b></div>
-                <div>Сумма заказа: <b>{order.price} руб</b></div>
-                <div>Имя: <b>{order.name}</b></div>
-                <div>Телефон: <b>{order.phone}</b></div>
-            </div>
+            <Container className="d-flex flex-column rounded border bg-white" style={{ width: "400px", marginTop: "100px" }}>
+                <h4 className="text-center mt-3">Ваш заказ успешно сформирован</h4>
+                <div className="my-4">
+                    <div>Номер заказа: <b>№{order.id}</b></div>
+                    <div>Сумма заказа: <b>{order.price} руб</b></div>
+                    <div>Имя: <b>{order.name}</b></div>
+                    <div>Телефон: <b>{order.phone}</b></div>
+                </div>
             </Container>
-            <h4 className="text-center mt-5">Состав заказа:</h4>
+            <h4 className="text-center mt-5">
+                Состав заказа: <b>{quantity}</b> изделий
+            </h4>
             <Row className="mb-5">
                 {order.order_products && order.order_products.map((product) => (
                     <OrderProductItem key={product.id} product={product} />
